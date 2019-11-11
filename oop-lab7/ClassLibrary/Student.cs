420267255fdf42d779fcc777c20f6bc26c3cc198
Nadya <vip.kovcunyak60@gmail.com>
2019-11-11 12:33:43 +0200

    7 практична

---
+++ oop-lab7/ClassLibrary/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Student : Abiturient
+    {
+        public int Kurs { set; get; }
+        public string Group { set; get; }
+        public string Fakultet { set; get; }
+        public string NavchZaklad { set; get; }
+
+        public Student() { }
+        public Student(int kurs, string group, string fakultet, string navchZaklad, Abiturient abit) : base(abit)
+        {
+            Kurs = kurs;
+            Group = group;
+            Fakultet = fakultet;
+            NavchZaklad = navchZaklad;
+        }
+        public Student(Student prevStudent) : base(prevAbit: prevStudent)
+        {
+            Kurs = prevStudent.Kurs;
+            Group = prevStudent.Group;
+            Fakultet = prevStudent.Fakultet;
+            NavchZaklad = prevStudent.NavchZaklad;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("\nStudent");
+            Console.WriteLine($"Kurs: {Kurs}");
+            Console.WriteLine($"Gruppa: {Group}");
+            Console.WriteLine($"Fakultet: {Fakultet}");
+            Console.WriteLine($"NavchZaklad: {NavchZaklad}");
+        }
+    }
+}
